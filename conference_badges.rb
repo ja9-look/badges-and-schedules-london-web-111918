60cf74a0a76ff0a25f52993cbbf3591eef97d65e
Janine Luk <janineluk95@gmail.com>
2018-11-12 16:36:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(badge_maker)
   badges = []
-  badges.push(attendees)
+  badges.push(badge_maker)
   return badges
 end
